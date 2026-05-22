5f62379d6b9476fdc56713473cdbeb10168254c0
renzetorino <148633398+renzetorino@users.noreply.github.com>
2026-05-22 21:58:16 +0800

    Renze TOrinooo

--- server/routers/index.ts
+++ server/routers/index.ts
@@ -2,11 +2,8 @@
  * This file contains the root router of tRPC-backend
  */
 
-import { cachedSession } from '@/lib/auth/utils-server';
-import { db } from '@/lib/db';
+
 import { inferRouterInputs, inferRouterOutputs, lazy } from '@trpc/server';
-import { cache } from 'react';
-import type { TRPCContext } from '../context';
 import { createCallerFactory, publicProcedure, router } from '../trpc';
 
 export const trpcRouter = router({
@@ -29,14 +26,9 @@ export type RouterOutputs = inferRouterOutputs<TRPCRouter>;
 /**
  * Create a server-side context for the tRPC API.
  */
-const createCallerContext = cache(
-  async (): Promise<TRPCContext> => ({
-    session: await cachedSession(),
-    db: db,
-  }),
-);
+
 
 /**
  * Create a server-side caller for the tRPC API.
  */
-export const caller = createCallerFactory(trpcRouter)(createCallerContext);
+export const caller = createCallerFactory(trpcRouter);
