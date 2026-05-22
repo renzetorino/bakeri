8b9497b2f71a3f912bcffa56c04be6708bb45a9b
renzetorino <148633398+renzetorino@users.noreply.github.com>
2026-05-22 21:41:22 +0800

    AHHH

---
+++ lib/auth/config.ts
@@ -0,0 +1,45 @@
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { randomUUID } from 'node:crypto';
+import { db } from '../db';
+import * as schema from '../db/schema/user-schema';
+
+export const auth = betterAuth({
+  baseURL: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+    schema: {
+      user: schema.userAccount,
+      account: schema.accounts,
+      session: schema.sessions,
+      verificationToken: schema.verificationTokens,
+    },
+  }),
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: false,
+  },
+  // socialProviders: {
+  //   google: {
+  //     clientId: process.env.GOOGLE_CLIENT_ID || '',
+  //     clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+  //   },
+  //   github: {
+  //     clientId: process.env.GITHUB_CLIENT_ID || '',
+  //     clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
+  //   },
+  // },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7, // 7 days
+    updateAge: 60 * 60 * 24, // 1 day
+  },
+  trustedOrigins: [
+      process.env.NEXT_PUBLIC_BETTER_AUTH_URL ||
+      'http://localhost:3000',
+  ],
+  advanced: {
+    database: {
+      generateId: () => randomUUID(),
+    },
+  },
+});
